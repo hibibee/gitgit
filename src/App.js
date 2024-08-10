97009ed24a08298082be1742ae8acbf831ac8fe9
hibibee <144363297+hibibee@users.noreply.github.com>
2024-08-10 17:47:18 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,15 +1,17 @@
 import React from 'react';
 
 import Navbar from './features/Navbar';
+import Container from './features/Container';
+import Home from './features/Home';
 
 function App() {
   return (
-    <div>
+    <>
       <Navbar />
-      <h1 style={{ padding: '4rem 0', margin: '0 auto', maxWidth: 1200 }}>
-        Hello React
-      </h1>
-    </div>
+      <Container>
+        <Home />
+      </Container>
+    </>
   );
 }
 
